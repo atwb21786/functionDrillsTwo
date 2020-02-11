c58ff281355bbb971923e4374fe1f09acb8c366c
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-02-10 21:17:31 -0700

    cracking the code

--- fnDrills2.js
+++ fnDrills2.js
@@ -4,7 +4,7 @@ function jediName(firstName, lastName) {
   return `${lastName.slice(0,3)}${firstName.slice(0,2)}`;
 }
 
-console.log(jediName("Beyonce", "Knowles"));
+console.log(jediName('Beyonce', 'Knowles'));
 
 function beyond(num) {
   if(num === Infinity) { console.log('And beyond'); }
@@ -14,3 +14,18 @@ function beyond(num) {
 }
 
 beyond(0);
+
+function decode(word) {
+  let code = ' ';
+  if(word.charAt(0) === 'a') { code = word.charAt(1) }
+  else if(word.charAt(0) === 'b') { code = word.charAt(2) }
+  else if(word.charAt(0) === 'c') { code = word.charAt(3) }
+  else if(word.charAt(0) === 'd') { code = word.charAt(4) }
+  else { return code; }
+  return code;
+  
+}
+
+let message = 'craft block argon meter bells brown croon droop';
+let newWord = message.split(' ').map( (word) => { return decode(word); }).join('');
+console.log(newWord);
